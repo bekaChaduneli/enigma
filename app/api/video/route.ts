c541a2d378488048e8c22dfa57229446e907447b
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-26 12:42:52 +0400

    add api limiter

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!,
 });
@@ -26,6 +28,12 @@ export async function POST(req: Request) {
             return new NextResponse("Prompt are required", { status: 400 });
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial has expired.", { status: 403 });
+        }
+
         const response = await replicate.run(
             "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
             {
@@ -34,6 +42,9 @@ export async function POST(req: Request) {
                 },
             }
         );
+
+        await increaseApiLimit();
+
         return NextResponse.json(response);
     } catch (error) {
         console.log("[VIDEO_ERROR]", error);
